abdc8811dd5fbf92347b9755d242034d908cdfc0
seanakdh <129484371+seanakdh@users.noreply.github.com>
2024-07-11 20:48:21 +0000

    Update main.go

--- example/main.go
+++ example/main.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	//Read files to depth 1, so all files contained in './' but not further
 	files, _ := watchit.GetDirFiles("./", 1)
-	//Create a response channel, maybe consider making it bufferd if expecting frequent changes
-	changedch := make(chan watchit.WatchItInfo, 100)
+	//Create a response channel, maybe consider making it bufferd if watching many files
+	changedch := make(chan watchit.WatchItInfo)
 	//Start watching the files with 0.1 second interval
 	watchit.WatchMultipleFilesAsync(files, changedch, 100)
 	//Just print the responses
